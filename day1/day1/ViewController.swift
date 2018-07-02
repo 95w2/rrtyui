347f9eab8890d353517f9015097e869f9469e90b
Brian Xu <35032810+W952@users.noreply.github.com>
2018-07-02 16:08:56 +0800

    added userdefaults

--- day1/day1/ViewController.swift
+++ day1/day1/ViewController.swift
@@ -23,38 +23,18 @@ class ViewController: UIViewController {
     var p1color: UIColor!
     var p2color: UIColor!
     
-    let settings = UIAlertController(title: "SETTINGS", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
-    let gamemodes = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.alert)
-    let customMax = UIAlertController(title: "SET MAX SCORE", message: nil, preferredStyle: UIAlertControllerStyle.alert)
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if UserDefaults.standard.object(forKey: "previousMax") != nil {
+            maxScore = UserDefaults.standard.integer(forKey: "previousMax")
+        }
+        
         p1color = p1button.backgroundColor!
         p2color = p2button.backgroundColor!
         
         p2scoreLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         p2button.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-        
-        settings.addAction(UIAlertAction(title: "CHANGE GAME MODE", style: .default, handler: {(alert: UIAlertAction!) in self.present(self.gamemodes, animated: true, completion: nil) }))
-        settings.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
-        
-        gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 20", style: .default, handler: {(alert: UIAlertAction!) in self.maxScore = 20 }))
-        gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 30", style: .default, handler: {(alert: UIAlertAction!) in self.maxScore = 30 }))
-        gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 50", style: .default, handler: {(alert: UIAlertAction!) in self.maxScore = 50 }))
-        gamemodes.addAction(UIAlertAction(title: "CUSTOM", style: .default, handler: {(alert: UIAlertAction!) in self.present(self.customMax, animated: true, completion: nil) }))
-        gamemodes.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
-        
-        customMax.addTextField(configurationHandler: { (textField) in
-            textField.keyboardType = .numberPad
-            textField.textAlignment = .center
-        })
-        customMax.addAction(UIAlertAction(title: "CONFIRM", style: .default, handler: {(alert: UIAlertAction!) in
-            let textField = self.customMax.textFields![0]
-            let newMax: Int? = Int(textField.text!)
-            self.maxScore = newMax!
-        }))
-        customMax.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
     }
 
     override func didReceiveMemoryWarning() {
@@ -65,37 +45,27 @@ class ViewController: UIViewController {
     @IBAction func p1buttonTapped(_ sender: UIButton) {
         p1score += 1
         p1button.setTitle(String(p1score), for: .normal)
+        sender.backgroundColor = p1button.backgroundColor!.darker(by: CGFloat(30/maxScore))
         
         if p1score == maxScore {
             p1button.setTitle("WINNER", for: .disabled)
             
-            p1button.isEnabled = false
-            p2button.isEnabled = false
-            resetButton.isHidden = false
-            resetButton.isEnabled = true
-            settingsButton.isHidden = false
-            settingsButton.isEnabled = true
+            disableTaps()
+            enableMenu()
         }
-        
-        sender.backgroundColor = p1button.backgroundColor!.darker(by: CGFloat(30/maxScore))
     }
     
     @IBAction func p2buttonTapped(_ sender: UIButton) {
         p2score += 1
         p2button.setTitle(String(p2score), for: .normal)
+        sender.backgroundColor = p2button.backgroundColor!.darker(by: CGFloat(30/maxScore))
         
         if p2score == maxScore {
             p2button.setTitle("WINNER", for: .disabled)
             
-            p2button.isEnabled = false
-            p1button.isEnabled = false
-            resetButton.isHidden = false
-            resetButton.isEnabled = true
-            settingsButton.isHidden = false
-            settingsButton.isEnabled = true
+            disableTaps()
+            enableMenu()
         }
-        
-        sender.backgroundColor = p2button.backgroundColor!.darker(by: CGFloat(30/maxScore))
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
@@ -123,7 +93,49 @@ class ViewController: UIViewController {
     }
     
     @IBAction func settingsButtonTapped(_ sender: UIButton) {
+        let settings = UIAlertController(title: "SETTINGS", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        settings.addAction(UIAlertAction(title: "CHANGE GAME MODE", style: .default, handler: {(alert: UIAlertAction!) in
+            let gamemodes = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.alert)
+            gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 20", style: .default, handler: {(alert: UIAlertAction!) in self.maxScore = 20 }))
+            gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 30", style: .default, handler: {(alert: UIAlertAction!) in self.maxScore = 30 }))
+            gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 50", style: .default, handler: {(alert: UIAlertAction!) in self.maxScore = 50 }))
+            gamemodes.addAction(UIAlertAction(title: "CUSTOM", style: .default, handler: {(alert: UIAlertAction!) in
+                let customMax = UIAlertController(title: "SET MAX SCORE", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+                customMax.addTextField(configurationHandler: { (textField) in
+                    textField.keyboardType = .numberPad
+                    textField.textAlignment = .center
+                })
+                customMax.addAction(UIAlertAction(title: "CONFIRM", style: .default, handler: {(alert: UIAlertAction!) in
+                    let textField = customMax.textFields![0]
+                    if let text = textField.text, !text.isEmpty
+                    {
+                        let newMax: Int? = Int(textField.text!)
+                        self.maxScore = newMax!
+                        UserDefaults.standard.set(self.maxScore, forKey: "previousMax")
+                    }
+                }))
+                customMax.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
+                
+                self.present(customMax, animated: true, completion: nil) }))
+            gamemodes.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
+            
+            self.present(gamemodes, animated: true, completion: nil) }))
+        settings.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
+        
         self.present(settings, animated: true, completion: nil)
+    }
+    
+    func enableMenu() {
+        resetButton.isHidden = false
+        resetButton.isEnabled = true
+        settingsButton.isHidden = false
+        settingsButton.isEnabled = true
+    }
+
+    func disableTaps() {
+        p2button.isEnabled = false
+        p1button.isEnabled = false
     }
     
 }
