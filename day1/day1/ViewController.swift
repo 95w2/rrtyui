19ee3163ff12502c897a72ce2e1882a3c205c764
Brian Xu <35032810+W952@users.noreply.github.com>
2018-07-03 11:29:03 +0800

    lots

--- day1/day1/ViewController.swift
+++ day1/day1/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import SwiftSpinner
 
 class ViewController: UIViewController {
     
@@ -24,13 +25,18 @@ class ViewController: UIViewController {
     var p1startColor: UIColor!
     var p2startColor: UIColor!
     
-    var player: AVAudioPlayer = AVAudioPlayer()
+    var maxDefaults = "previousMax"
+    var firstGameStart = true
+    
+    lazy var player: AVAudioPlayer = {
+        return AVAudioPlayer()
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if UserDefaults.standard.object(forKey: "previousMax") != nil {
-            maxScore = UserDefaults.standard.integer(forKey: "previousMax")
+        if UserDefaults.standard.object(forKey: maxDefaults) != nil {
+            maxScore = UserDefaults.standard.integer(forKey: maxDefaults)
         }
         
         p1startColor = p1button.backgroundColor!
@@ -39,17 +45,12 @@ class ViewController: UIViewController {
         p2scoreLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         p2button.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func p1buttonTapped(_ sender: UIButton) {
         p1score += 1
         p1button.setTitle(String(p1score), for: .normal)
         sender.backgroundColor = p1button.backgroundColor!.darker(by: CGFloat(30/maxScore))
-        playSound(named: "tap")
+        player.play()
         
         if p1score == maxScore {
             p1button.setTitle("WINNER", for: .disabled)
@@ -61,7 +62,7 @@ class ViewController: UIViewController {
         p2score += 1
         p2button.setTitle(String(p2score), for: .normal)
         sender.backgroundColor = p2button.backgroundColor!.darker(by: CGFloat(30/maxScore))
-        playSound(named: "tap")
+        player.play()
         
         if p2score == maxScore {
             p2button.setTitle("WINNER", for: .disabled)
@@ -70,61 +71,55 @@ class ViewController: UIViewController {
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
-        if sender == resetButton {
-            if resetButton.titleLabel!.text == "START" {
-                resetButton.setTitle("RESET", for: .normal)
+        
+        if firstGameStart == true {
+            guard let soundPath = Bundle.main.path(forResource: "tap", ofType: "wav") else {
+                return
             }
-            resetButton.isHidden = true
-            resetButton.isEnabled = false
-            settingsButton.isHidden = true
-            settingsButton.isEnabled = false
+            let soundURL = NSURL(fileURLWithPath: soundPath) as URL
+            player = try! AVAudioPlayer(contentsOf: soundURL, fileTypeHint: AVFileType.wav.rawValue)
+            
+            resetButton.setTitle("RESET", for: .normal)
+            firstGameStart = false
+            
+        }
         
-            p1score = 0
-            p1button.isEnabled = true
-            p1button.setTitle(String(p1score), for: .normal)
-            p1button.setTitle(String(p1score), for: .disabled)
-            p1button.backgroundColor = p1startColor
+        resetButton.isHidden = true
+        resetButton.isEnabled = false
+        settingsButton.isHidden = true
+        settingsButton.isEnabled = false
         
-            p2score = 0
-            p2button.isEnabled = true
-            p2button.setTitle(String(p2score), for: .normal)
-            p2button.setTitle(String(p2score), for: .disabled)
-            p2button.backgroundColor = p2startColor
+        p1score = 0
+        p1button.setTitle(String(p1score), for: .normal)
+        p1button.setTitle(String(p1score), for: .disabled)
+        p1button.backgroundColor = p1startColor
+        
+        p2score = 0
+        p2button.setTitle(String(p2score), for: .normal)
+        p2button.setTitle(String(p2score), for: .disabled)
+        p2button.backgroundColor = p2startColor
+        
+        SwiftSpinner.show("3")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            SwiftSpinner.show("2")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            SwiftSpinner.show("1")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            SwiftSpinner.show("GO")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            SwiftSpinner.hide()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.p1button.isEnabled = true
+            self.p2button.isEnabled = true
         }
     }
     
     @IBAction func settingsButtonTapped(_ sender: UIButton) {
-        let settings = UIAlertController(title: "SETTINGS", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
-        
-        settings.addAction(UIAlertAction(title: "CHANGE GAME MODE", style: .default, handler: {(alert: UIAlertAction!) in
-            let gamemodes = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.alert)
-            gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 20", style: .default, handler: {(alert: UIAlertAction!) in self.maxScore = 20 }))
-            gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 30", style: .default, handler: {(alert: UIAlertAction!) in self.maxScore = 30 }))
-            gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 50", style: .default, handler: {(alert: UIAlertAction!) in self.maxScore = 50 }))
-            gamemodes.addAction(UIAlertAction(title: "CUSTOM", style: .default, handler: {(alert: UIAlertAction!) in
-                let customMax = UIAlertController(title: "SET MAX SCORE", message: nil, preferredStyle: UIAlertControllerStyle.alert)
-                customMax.addTextField(configurationHandler: { (textField) in
-                    textField.keyboardType = .numberPad
-                    textField.textAlignment = .center
-                })
-                customMax.addAction(UIAlertAction(title: "CONFIRM", style: .default, handler: {(alert: UIAlertAction!) in
-                    let textField = customMax.textFields![0]
-                    if let text = textField.text, !text.isEmpty
-                    {
-                        let newMax: Int? = Int(textField.text!)
-                        self.maxScore = newMax!
-                        UserDefaults.standard.set(self.maxScore, forKey: "previousMax")
-                    }
-                }))
-                customMax.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
-                
-                self.present(customMax, animated: true, completion: nil) }))
-            gamemodes.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
-            
-            self.present(gamemodes, animated: true, completion: nil) }))
-        settings.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
-        
-        self.present(settings, animated: true, completion: nil)
+        createSettings()
     }
     
     func enableMenu() {
@@ -144,11 +139,44 @@ class ViewController: UIViewController {
         disableTaps()
     }
     
-    @discardableResult func playSound(named soundName: String) -> AVAudioPlayer {
-        let soundURL = Bundle.main.path(forResource: soundName, ofType: "wav")
-        player = try! AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: soundURL!) as URL, fileTypeHint: AVFileType.wav.rawValue)
-        player.play()
-        return player
+    func createSettings() {
+        let settings = UIAlertController(title: "SETTINGS", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        settings.addAction(UIAlertAction(title: "CHANGE GAME MODE", style: .default, handler: { (alert: UIAlertAction!) in self.createGamemodes() }))
+        settings.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
+        self.present(settings, animated: true, completion: nil)
+    }
+    
+    func createGamemodes() {
+        let gamemodes = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.alert)
+        
+        gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 20", style: .default, handler: { (alert: UIAlertAction!) in self.maxScore = 20 }))
+        gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 30", style: .default, handler: { (alert: UIAlertAction!) in self.maxScore = 30 }))
+        gamemodes.addAction(UIAlertAction(title: "MAX SCORE: 50", style: .default, handler: { (alert: UIAlertAction!) in self.maxScore = 50 }))
+        gamemodes.addAction(UIAlertAction(title: "CUSTOM", style: .default, handler: { (alert: UIAlertAction!) in self.createCustomMax() }))
+        gamemodes.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
+        self.present(gamemodes, animated: true, completion: nil)
+    }
+    
+    func createCustomMax() {
+        let customMax = UIAlertController(title: "SET MAX SCORE", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+        
+        customMax.addTextField(configurationHandler: { (textField) in
+            textField.keyboardType = .numberPad
+            textField.textAlignment = .center
+        })
+        customMax.addAction(UIAlertAction(title: "CONFIRM", style: .default, handler: {(alert: UIAlertAction!) in
+            let textField = customMax.textFields![0]
+            if let text = textField.text, !text.isEmpty
+            {
+                let newMax: Int? = Int(textField.text!)
+                self.maxScore = newMax!
+                UserDefaults.standard.set(self.maxScore, forKey: self.maxDefaults)
+            }
+        }))
+        customMax.addAction(UIAlertAction(title: "< BACK", style: .default, handler: nil))
+        self.present(customMax, animated: true, completion: nil)
     }
 }
+
 
